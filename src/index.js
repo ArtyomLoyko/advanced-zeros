4bf96ca97a7353a096139f00c2792ccd9ef0eaf8
Artyom <artloer98@gmail.com>
2018-02-28 03:29:04 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var a = base, arr = [];
+
+  for(var i = 2; i <= 256; i++) {
+    if(a % i == 0) {
+      arr.push(i);
+      a = a / i--;
+    };
+  };
+
+  if(arr.length == 1) {
+    var x = base;
+  };
+
+  var sumMinElem = 0, sumMaxElem = 0, minElem = Math.min(...arr), maxElem = Math.max(...arr);
+  for(var i = 0; i < arr.length; i++) {
+    if(minElem == arr[i]) sumMinElem += arr[i];
+    if(maxElem == arr[i]) sumMaxElem += arr[i];
+  };
+
+  if(sumMinElem >= 2 * sumMaxElem) x = minElem;
+  else x = maxElem;
+
+  var counter = 0, counterOfPower = 0;
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] == x) counterOfPower++;
+  };
+
+  for(var i = x; i <= number; i *= x) {
+    counter += Math.floor(number / i);
+  };
+
+  if(counterOfPower) return Math.floor(counter / counterOfPower);
+  return counter;
 }
